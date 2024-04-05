ce9636c24d749380712fa85409823985ff83641c
Fastaxx <Fastaxx@github.com>
2024-04-05 09:49:38 +0200

    modify exampl

--- examples/example.jl
+++ examples/example.jl
@@ -7,8 +7,8 @@ mesh = generate_mesh(grid)
 domain = ((minimum(mesh[1]), maximum(mesh[1])), (minimum(mesh[2]), maximum(mesh[2])))
 
 # SignedDistanceFunction
-circle_function = (x, y) -> sqrt(x^2 + y^2) - 1.0
-square_function = (x, y) -> max(abs(x), abs(y)) - 1.0
+circle_function = (x, y) -> sqrt(x^2 + y^2) - 0.5
+square_function = (x, y) -> max(abs(x), abs(y)) - 0.5
 identity_transform = (x, y, t) -> (x, y)
 
 # Create Geometry
@@ -19,11 +19,22 @@ square_sdf = SignedDistanceFunction(square_function, identity_transform, domain,
 union_sdf = circle_sdf ⊔ square_sdf
 
 # Transformation function
-move_transform = (x, y, t) -> (x + t, y)
+move_transform = (x, y, t) -> ((x-0.25) + 2*t, (y-0.25))
 
 # Move the union of two signed distance functions
 moving_sdf = SignedDistanceFunction(union_sdf.sdf_function, move_transform, union_sdf.domain, true)
 
+# Print the normal vector at a given point
+println("Normal au point (0.5, 0.5) pour le cercle", normal(circle_sdf.sdf_function, (0.5, 0.5)))
+
+# Calculer le vecteur tangentiel
+tangent = tangent_vector(normal(circle_sdf.sdf_function, (0.5, 0.5)))
+println("Vecteur tangentiel au point (0.5, 0.5) pour le cercle", tangent)
+
+# Calculer la courbure pour le cercle
+println("Courbure pour le cercle au point (0.5, 0.5)", curvature(circle_function.sdf_function, (0.5, 0.5)))
+
+# Plot
 t=0.0
 plot_sdf(moving_sdf, domain, t)
 readline()
